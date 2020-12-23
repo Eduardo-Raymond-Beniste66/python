aee5fba2609bd09cb6a8cfac96fdce36afb526e7
Eduardo Raymond Beniste <76535927+Eduardo-Raymond-Beniste66@users.noreply.github.com>
2020-12-23 20:48:17 -0300

    ExercicioLista2Exercicio5
    >duardo Raymond Beniste

---
+++ ExercicioLista2Exercicio5.py
@@ -0,0 +1,15 @@
+a = int(input('a:'))
+b = int(input('b:'))
+c = int(input('c:'))
+if a >= b and a >= c:
+ print(f'Maior: {a}')
+elif  b >= c:
+    print(f'Mamior: {b}')
+else:
+    print(f'Maior: {b}')
+if a >= b:
+    print(f'Maior: {a}')
+if b >= c:
+    print(f'Maior: {b}')
+if c >=  b:
+    print(f'Maior: {c}')
