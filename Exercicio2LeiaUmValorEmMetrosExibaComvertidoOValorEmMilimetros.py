bb7f721b9198d3eb240d5c016b3395bb8ac20d89
Eduardo Raymond Beniste <76535927+Eduardo-Raymond-Beniste66@users.noreply.github.com>
2020-12-23 20:32:13 -0300

    Exercicio2LeiaUmValorEmMetrosExibaComvertidoOValorEmMilimetros
    Eduardo Raymond Beniste

---
+++ Exercicio2LeiaUmValorEmMetrosExibaComvertidoOValorEmMilimetros.py
@@ -0,0 +1,9 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: D:/Trabalhar na Manifest Pyinton/Exercicio2LeiaUmValorEmMetrosExibaComvertidoOValorEmMilimetros.py
+print('-----converter de Metros para Mmilimetros------')
+
+n1=float(inpu('valor em metros:') replece('.','.'))
+n2=n1+1000
+print('O valor em é: %.2f mimlimetros '%n2')
