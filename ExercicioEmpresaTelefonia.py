a76d82cfd9a9cc8970fbe9f7677598bbc2160416
Eduardo Raymond Beniste <76535927+Eduardo-Raymond-Beniste66@users.noreply.github.com>
2020-12-23 20:39:02 -0300

    ExercicioEmpresaTelefonia
    Eduardo Raymond Beniste

---
+++ ExercicioEmpresaTelefonia.py
@@ -0,0 +1,15 @@
+  
+    telefonia = float(input('Valor por ligação:'))
+    if telefonia <= 200:
+        print('Valro menor de 200')
+    if telefonia >= 270:
+        print('Valor acima de  27-  por minuto')
+    if telefonia >= 400:
+        print('o valor é de cobrado acima de 400 por minuto {telefonia.0.8f')
+    print('----O Valor a cer Cobrado por minuto acima de 400 do telefone e:----')
+    telefonia = float(input("Digite o valor acima de R$ 400,00: e:'))
+    if telefonia >= 400:
+        telefone = (200 * 0.20 + 270 + 400 * 0.8 400 *9.15)
+        print('O valor cobrado acima de 400 e:')
+    else:
+        print('Fim do programa')
