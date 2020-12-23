ef7cc2b4a913a49d22ffec61d44b04e5203b77bf
Eduardo Raymond Beniste <76535927+Eduardo-Raymond-Beniste66@users.noreply.github.com>
2020-12-23 20:46:16 -0300

    ExercicioLista2Exercicio4
    Eduardo raymond Beniste

---
+++ ExercicioLista2Exercicio4.py
@@ -0,0 +1,9 @@
+a = int(input('a:'))
+b = int(input('b:'))
+c = int(input('c:'))
+if a >= b and a == 4:
+    print(f'Maior: {a}')
+elif b >= c:
+    print('maior: [b}')
+else:
+    print(f'Maior: {c}')
