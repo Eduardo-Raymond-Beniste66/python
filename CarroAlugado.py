c69814ce2a93743ddb8ecda781a201de93287a8f
Eduardo Raymond Beniste <76535927+Eduardo-Raymond-Beniste66@users.noreply.github.com>
2020-12-23 20:15:27 -0300

    CarroAlugado
    Exercício de python Carro Alugado

---
+++ CarroAlugado.py
@@ -0,0 +1,13 @@
+print('----CarroAlugado----')
+
+carro=float(input('Alugar o carro:'))
+print(carro)
+
+QTDKLP=float(input('Digite a quantidade de Kilometro percorrido:'))
+print(QTDKLP)
+QTDDasP=float(input('Quantidades de dias  do carro alugado:'))
+print(QTDDasP)
+
+PrecoApagar=float(input('Preço apagar:'))
+
+PrecoApagar = carro == (60.00 * 0.15)
