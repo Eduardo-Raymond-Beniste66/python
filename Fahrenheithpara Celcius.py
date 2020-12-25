9f388154862fc6f4311c0d55977d57812201985a
Eduardo Raymond Beniste <76535927+Eduardo-Raymond-Beniste66@users.noreply.github.com>
2020-12-25 13:22:04 -0300

    Fahrenheithpara Celcius
    Eduardo Raymond Beniste

---
+++ Fahrenheithpara Celcius.py
@@ -0,0 +1,23 @@
+print('-----Calcule A Temperatura De Fahrenhet para Celcius-----')
+
+temperatura=int(input('Digite a Temperatura Em Celcios'))
+print(temperatura)
+
+
+F=int(input('Digite a temperatura Farenhight:'))
+print(F)
+
+C=int(input('Digite a temperatura Celcios:'))
+print(C)
+
+
+
+temperatura = F=9*C/5+32           
+
+F=int(input('A temperatura de Farenhight e:'  ))
+print(F)
+
+C=int(input('A tempreratura de Celcios e:'))
+print(C)
+
+
