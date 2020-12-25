4181bbdf27b4ca5776e2fdcc09b6daa885c01799
Eduardo Raymond Beniste <76535927+Eduardo-Raymond-Beniste66@users.noreply.github.com>
2020-12-25 13:20:19 -0300

    ExercicioQueConverteSelciosParaFirenhait
    Eduardo Raymond Beniste

---
+++ ExercicioQueConverteSelciosParaFirenhait.py
@@ -0,0 +1,24 @@
+print('-----Calcule A Temperatura De Celcio para-----')
+
+temperatura=int(input('Digite a Temperatura Em Celcios'))
+print(temperatura)
+
+
+
+C=int(input('Digite a temperatura Celcios:'))
+print(C)
+
+F=int(input('Digite a temperatura Farenhight:'))
+print(F)
+
+temperatura = F=9*C/5+32           
+
+F=int(input('A temperatura de Farenhight e:'  ))
+print(F)
+
+C=int(input('A tempreratura de Celcios e:'))
+print(C)
+
+
+
+
